9867d12fc8bc895cef073344599821616cfa99e7
Dennis <76845672+DennisNeu@users.noreply.github.com>
2025-06-10 20:04:58 +0200

    Setup first view

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the tracker index. This is the main app of the tracker project. And here the main view will reside.")
