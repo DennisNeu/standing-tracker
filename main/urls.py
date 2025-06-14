3015d09828ab684dc972f7c24ffbdf4fa7804137
Dennis <76845672+DennisNeu@users.noreply.github.com>
2025-06-14 21:07:45 +0200

    Add template

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,4 @@ from . import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('tracker.urls')),
-
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def tracker(request):
     return HttpResponse("Hello world! This is the test view of the tracker app. It is used to verify that the app is working correctly. If you see this message, the app is running fine.")
 def index(request):
-    return HttpResponse("Hello, world. You're at the tracker index. This is the main app of the tracker project. And here the main view will reside.")
+    template = loader.get_template('test.html')
+    return HttpResponse(template.render())
