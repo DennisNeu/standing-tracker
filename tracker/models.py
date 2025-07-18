b7f56f1a55127d635f227afe65400f94da66dc07
Dennis <76845672+DennisNeu@users.noreply.github.com>
2025-07-18 18:20:05 +0200

    Change model

--- tracker/models.py
+++ tracker/models.py
@@ -3,10 +3,9 @@ from django.db import models
 # Create your models here.
 
 
-class Session(models.Model):
-    session_id = models.CharField(max_length=100, unique=True)
-    start_time = models.DateTimeField(auto_now_add=True)
-    end_time = models.DateTimeField(null=True, blank=True)
+class StandingTime(models.Model):
+    time = models.DurationField(max_length=20)  # Or use models.DurationField if
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Session {self.session_id} from {self.start_time} to {self.end_time if self.end_time else 'ongoing'}"
