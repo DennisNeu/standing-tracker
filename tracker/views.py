0dbd254d618e3f256720ad5f0d9e574bb5a587eb
Dennis <76845672+DennisNeu@users.noreply.github.com>
2025-07-24 19:40:32 +0200

    Add basic history query

--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.db.models import Sum
+from django.db.models import Sum, Count
+from django.db.models.functions import TruncDate
 from django.views.decorators.csrf import csrf_exempt
 
 import json
@@ -11,8 +12,9 @@ from .models import StandingTime
 def index(request):
     highscore = StandingTime.objects.all().order_by('-time').first() or StandingTime(time=0)
     total = StandingTime.objects.aggregate(Sum('time'))['time__sum'] or 0
+    history = get_daily_history_qs()
     print(f"highscore: {highscore.time}; total: {total}")
-    return render(request, 'index.html', {'highscore': highscore.time, 'total': total})
+    return render(request, 'index.html', {'highscore': highscore.time, 'total': total, 'history': history})
 
 
 @csrf_exempt  # Use this decorator to allow POST requests without CSRF token
@@ -31,3 +33,15 @@ def save_standing_time(request):
         except Exception as e:
             return JsonResponse({'status': 'error', 'message': str(e)}, status=400)
     return JsonResponse({'status': 'error', 'message': 'Invalid request method'}, status=405)
+
+def get_daily_history_qs():
+    return (
+        StandingTime.objects.all()
+        .annotate(day=TruncDate('created_at'))          # group key
+        .values('day')
+        .annotate(
+            total_seconds=Sum('time'),
+            entries=Count('id'),
+        )
+        .order_by('-day')
+    )
